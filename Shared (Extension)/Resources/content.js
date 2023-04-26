6012cc966f0dfa3095b269152f13a928ffd0a88b
Pat Nakajima <patnakajima@gmail.com>
2023-04-26 13:14:06 -0700

    first

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -5,3 +5,12 @@ browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log("Received request: ", request);
 });
+
+setInterval(function() {
+	const children = document.querySelector('#bottom-sheet-sensor').parentElement.children
+	for (const child of children) {
+		if (child.hasAttribute('style')) {
+			child.remove()
+		}
+	}
+}, 1000);
